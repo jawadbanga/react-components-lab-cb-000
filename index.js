3a70d65ae308d65d7c6afc0a965c1d09d49162fc
Jawad Bangash <jawadbanga@gmail.com>
2017-02-22 11:00:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const React = require('react')
+const ReactDom = requre('reactdom')
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
   'Beef shin',
   'Ribeye'
 ];
+
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', {className:'oldercoaster'}, [React.createElement('p', {}, 'Two grannies having the time of their life!'),
+    React.createElement('p', {}, 'Passengers:'), React.createElement('ul', {}, [React.createElement('li', {}, 'Agnes'), React.createElement('li', {}, 'Muriel')]) ]);
+  }
+})
+
+const InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {}, [React.createElement('p', {}, 'You shouldn't look too far.'),
+    React.createElement('p', {}, 'Sometimes, the solution is '  )
+    ]);
+  }
+}
 
 ReactDOM.render(
   React.createElement('div', {}, [
